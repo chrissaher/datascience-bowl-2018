23a2aab7c24727503c13707abbd7c8d655667c66
Christian Saravia <csaravia@Christian.local>
2018-02-23 14:48:26 +0900

    Fixed error of relative folders

--- utils/constants.py
+++ utils/constants.py
@@ -1,11 +1,11 @@
 import re
 
-STAGE1_TRAIN_PATH = "./data-raw/stage1_train"
-STAGE1_TEST_PATH = "./data-raw/stage1_test"
-IMAGES_PATH = "./data-raw/stage1_train/:train_id/images"
-MASKS_PATH = "./data-raw/stage1_train/:train_id/masks"
-IMAGE_PATH = "./data-raw/stage1_train/:train_id/images/:image_id"
-MASK_PATH = "./data-raw/stage1_train/:train_id/images/:mask_id"
+STAGE1_TRAIN_PATH = "../data-raw/stage1_train"
+STAGE1_TEST_PATH = "../data-raw/stage1_test"
+IMAGES_PATH = "../data-raw/stage1_train/:train_id/images"
+MASKS_PATH = "../data-raw/stage1_train/:train_id/masks"
+IMAGE_PATH = "../data-raw/stage1_train/:train_id/images/:image_id"
+MASK_PATH = "../data-raw/stage1_train/:train_id/images/:mask_id"
 RE_TRAIN_ID = re.compile(":train_id")
 RE_IMAGE_ID = re.compile(":image_id")
 RE_MASK_ID = re.compile(":mask_id")
